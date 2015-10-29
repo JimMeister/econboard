de1c0b187f3d5a799d05ee32fdd411e630e51153
Sébastien Villemot <sebastien.villemot@sciencespo.fr>
2015-10-29 14:07:32 +0100

    Fix bug in eurostat.nominal.gdp.euro().

--- R/eurostat.R
+++ R/eurostat.R
@@ -35,7 +35,7 @@ eurostat.growth <- function(countries, startPeriod, endPeriod, frequency = 1)
 ## Nominal GDP in euros
 eurostat.nominal.gdp.euro <- function(countries, startPeriod, endPeriod, frequency = 1)
   eurostat.query("nama_10_gdp", list(FREQ = "A", UNIT = "CP_MEUR",
-                                     NA_ITEM = "B1G", GEO = countries),
+                                     NA_ITEM = "B1GQ", GEO = countries),
                  startPeriod, endPeriod, frequency) * 1e6
 
 eurostat.unemployment <- function(countries, startPeriod, endPeriod, frequency = 1)
